e82508a9eda81bcce06ec490f7d6f1df2721a4c3
fadiasali <fadiasali2002@gmail.com>
2024-05-09 16:49:39 +0300

    I But Border And I made them 100 questions

--- js-version/custom.js
+++ js-version/custom.js
@@ -1,21 +1,41 @@
-document.addEventListener('DOMContentLoaded', function () {
+// document.addEventListener('DOMContentLoaded', function () {
 
+// 	document.querySelector('#btn-one').addEventListener('click', function () {
+// 		html2canvas(document.querySelector('#content')).then((canvas) => {
+// 			let base64image = canvas.toDataURL('image/png');
+
+// 			let pdfWidth = 395.28;
+// 			let pdfHeight = 841.89;
+
+// 			let pdf = new jsPDF('p', 'px', [pdfWidth, pdfHeight]);
+// 			let imageWidth = pdfWidth - 30;
+// 			let imageHeight = (imageWidth * canvas.height) / canvas.width
+
+// 			pdf.addImage(base64image, 'PNG', 15, 15, imageWidth, imageHeight);
+// 			pdf.save('pdf');
+// 		});
+// 	});
+// });
+
+
+document.addEventListener('DOMContentLoaded', function () {
 	document.querySelector('#btn-one').addEventListener('click', function () {
 		html2canvas(document.querySelector('#content')).then((canvas) => {
 			let base64image = canvas.toDataURL('image/png');
 
-			let pdfWidth = 395.28;
-			let pdfHeight = 841.89;
+			let pdfWidth = 24 * 37.8; // تحويل العرض من سم إلى بكسل (باستخدام عامل التحويل 37.8 بكسل في السم)
+			let pdfHeight = 29.7 * 37.8; // تحويل الارتفاع من سم إلى بكسل (باستخدام عامل التحويل 37.8 بكسل في السم)
 
 			let pdf = new jsPDF('p', 'px', [pdfWidth, pdfHeight]);
 			let imageWidth = pdfWidth - 30;
-			let imageHeight = (imageWidth * canvas.height) / canvas.width
+			let imageHeight = (imageWidth * canvas.height) / canvas.width;
 
 			pdf.addImage(base64image, 'PNG', 15, 15, imageWidth, imageHeight);
 			pdf.save('pdf');
 		});
 	});
 });
+
 
 
 
@@ -32,14 +52,17 @@ function createQuestions() {
 
 
 
-		var left_div = createCircleContainer("col-md-4 left_div");
+		var left_div = createCircleContainer("col-md-3 left_div");
 		main_div.appendChild(left_div);
 
-		var center_div = createCircleContainer("col-md-4 center_div");
+		var center_div = createCircleContainer("col-md-3 center_div");
 		main_div.appendChild(center_div);
 
-		var right_div = createCircleContainer("col-md-4 right_div");
-		main_div.appendChild(right_div);	
+		var center2_div = createCircleContainer("col-md-3 center2_div");
+		main_div.appendChild(center2_div);
+
+		var right_div = createCircleContainer("col-md-3 right_div");
+		main_div.appendChild(right_div);
 
 
 		var left_ul = createCircleContainerUl("left");
@@ -47,6 +70,9 @@ function createQuestions() {
 
 		var center_ul = createCircleContainerUl("center");
 		center_div.appendChild(center_ul);
+
+		var center2_ul = createCircleContainerUl("center2");
+		center2_div.appendChild(center2_ul);
 
 		var right_ul = createCircleContainerUl("right");
 		right_div.appendChild(right_ul);
@@ -61,7 +87,7 @@ function createQuestions() {
 
 
 		function createCircleContainerUl(className) {
-			var circleContainerul = document.createElement("ul");
+			var circleContainerul = document.createElement("li");
 			circleContainerul.className = className;
 			return circleContainerul;
 		}
@@ -85,14 +111,17 @@ function createQuestions() {
 
 		var columnClass;
 		for (var i = 1; i <= num_of_elements; i++) {
-			if (i < 34) {
+			if (i < 26) {
 				columnClass = left_ul;
-			} else if (i < 67) {
+			} else if (i < 51) {
 				columnClass = center_ul;
-			} else if (i < 100) {
+			}
+			else if (i < 76) {
+				columnClass = center2_ul;
+			} else if (i < 101) {
 				columnClass = right_ul;
 			}
-			else{
+			else {
 				return false;
 			}
 
